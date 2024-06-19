e0684e89ab7f29833ea4101a68549d60912ce58d
Susana Gallegos <mandalagc@gmail.com>
2024-06-18 19:26:22 -0600

    18-jun-24 / Home

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const images = [
+        '../assets/images/bck.webp', // Corrected paths
+        '../assets/images/v2.webp',
+        '../assets/images/cat1.jpg'
+    ];
+    let currentImageIndex = 0;
+    const banner = document.querySelector('.banner');
+    
+    function changeImage() {
+        const currentImage = banner.querySelector('img.active');
+        currentImage.classList.remove('active');
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        const nextImage = banner.querySelector(`img[data-index="${currentImageIndex}"]`);
+        nextImage.classList.add('active');
+    }
+
+    images.forEach((src, index) => {
+        const img = document.createElement('img');
+        img.src = src;
+        img.setAttribute('data-index', index);
+        if (index === 0) img.classList.add('active');
+        banner.appendChild(img);
+    });
+
+    setInterval(changeImage, 3000);
+});
